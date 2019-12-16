63b9002b914900721f072c5484daa79ce899577d
sovan <sovanshahihero@gmail.com>
2019-12-16 15:10:23 +0545

    Updates

---
+++ precache-manifest.7fd632dbf6eeec1d798eaad89168e5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0269fe8612dc3eaeca67747a12f226d3",
+    "url": "/react-list/index.html"
+  },
+  {
+    "revision": "1def4aa9c040d317037b",
+    "url": "/react-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f5772599d13951edcadd",
+    "url": "/react-list/static/js/2.d7617fc2.chunk.js"
+  },
+  {
+    "revision": "f4dd7e180a9fb5b68427e72adf0f87cc",
+    "url": "/react-list/static/js/2.d7617fc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1def4aa9c040d317037b",
+    "url": "/react-list/static/js/main.ac95b0bf.chunk.js"
+  },
+  {
+    "revision": "ffd1e6b075219d197707",
+    "url": "/react-list/static/js/runtime-main.47dc4491.js"
+  }
+]);
